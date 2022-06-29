9c38a782882a6723ef2e7d9fc74564353cbe397f
7995643998 <96734652+7995643998@users.noreply.github.com>
2022-06-29 17:07:05 +0530

    N=int(input()) for i in range(1,N+1):     for j in range(1,N+1):         if(j==1 and i<=N) or(i==N and j<=N):             print('*',end='')         elif(i==j):             print('*',end='')         else:             print(' ',end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,10 @@
 N=int(input())
-for i in range(N,0,-1):
+for i in range(1,N+1):
     for j in range(1,N+1):
-        if i==j or i>j:
-            print(chr(64+i),end=' ')
+        if(j==1 and i<=N) or(i==N and j<=N):
+            print('*',end='')
+        elif(i==j):
+            print('*',end='')
+        else:
+            print(' ',end='')
     print()
