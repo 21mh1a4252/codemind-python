3fc7c680172a04bf6fd100247c706a2d5e5da6fe
7995643998 <96734652+7995643998@users.noreply.github.com>
2022-07-06 20:06:41 +0530

    def dig(n):     c=0     if n==0:         return 1     elif n<0:         n = n*-1     while(n):         n=n//10         c+=1     return c x , y = map(int,input().split()) l = list(map(int,input().split())) a=[] for i in range(x):    a.append(dig(l[i])) print(a.count(y))

--- K_digit_length_numbers_in_a_array.py
+++ K_digit_length_numbers_in_a_array.py
@@ -1,20 +1,16 @@
 def dig(n):
+    c=0
     if n==0:
         return 1
-    c=0
-    if n<0:
-        n=-1*n
-    while n:
+    elif n<0:
+        n = n*-1
+    while(n):
         n=n//10
         c+=1
     return c
-n,m=map(int,input().split())
-a=list(map(int,input().split()))
-l=[]
-c=0
-for i in range(n):
-    l.append(dig(a[i]))
-for i in range(n):
-    if dig(a[i])==m:
-        c+=1
-print(c)
+x , y = map(int,input().split())
+l = list(map(int,input().split()))
+a=[]
+for i in range(x):
+   a.append(dig(l[i]))
+print(a.count(y))
